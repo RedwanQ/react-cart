3cac09c1d35a1f673b985c8d8a1aee5b8d14c968
Brian <bstanton773@gmail.com>
2021-10-26 15:27:36 -0500

    Racer form

--- src/views/Racers.js
+++ src/views/Racers.js
@@ -1,18 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import RacerDetail from '../components/RacerDetail';
+import { RacerForm } from '../components/RacerForm';
 
 export default function Racers(props){
     const [racers, setRacers] = useState([]);
+    const [season, setSeason] = useState(2021)
+    const [round, setRound] = useState(1)
 
     useEffect(() => {
-        fetch('http://ergast.com/api/f1/2021/1/driverStandings.json')
+        console.log(season, round)
+        fetch(`http://ergast.com/api/f1/${season}/${round}/driverStandings.json`)
             .then(res => res.json())
             .then(data => setRacers(data.MRData.StandingsTable.StandingsLists[0].DriverStandings))
     })
 
+    const handleForm = (e) => {
+        e.preventDefault();
+        console.log("Handling form!");
+        const seasonInput = e.target.season.value;
+        const roundInput = e.target.round.value
+        setSeason(seasonInput);
+        setRound(roundInput);
+    }
+
     return (
         <div>
             This is the Racers Page
+            <RacerForm handleForm={handleForm}/>
             {racers.length ? 
                 <table className="table table-dark table-striped">
                 <thead>
