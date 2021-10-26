204b667890dfba646c16b0c8716ebf85c7db5ca1
Brian <bstanton773@gmail.com>
2021-10-26 13:51:45 -0500

    useState

--- src/components/ButtonNameChange.js
+++ src/components/ButtonNameChange.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const ButtonNameChange = (props) => {
+    let myName = props.name;
+    const [name, setName] = useState(myName)
     const handleForm = (e) => {
         e.preventDefault();
         console.log(e)
@@ -9,11 +11,12 @@ export const ButtonNameChange = (props) => {
         props.changeName(newName);
     }
 
+
     return (
         <form onSubmit={handleForm}>
             <div className="mb-3">
                 <label htmlFor="newName" className="form-label">New Name</label>
-                <input type="text" className="form-control" id="newName" />
+                <input type="text" className="form-control" id="newName" value={name} onChange={(e) => setName(e.target.value)}/>
             </div>
             <input type='submit' value='Submit' className='btn btn-secondary' />
         </form>
