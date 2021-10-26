366e4d32e4bab752570fd4bd2eb96210de480d50
Brian <bstanton773@gmail.com>
2021-10-26 14:35:31 -0500

    Updated racers with useState and useEffect

--- src/views/Racers.js
+++ src/views/Racers.js
@@ -1,32 +1,37 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import RacerDetail from '../components/RacerDetail';
 
-export default class Racers extends Component {
-    constructor(props){
-        super(props);
-        console.log('Racers component constructed')
-        this.state = {
-            racers: []
-        }
-    }
+export default function Racers(props){
+    const [racers, setRacers] = useState([]);
 
-    componentDidMount(){
-        console.log('Racers component did mount')
+    useEffect(() => {
         fetch('http://ergast.com/api/f1/2021/1/driverStandings.json')
             .then(res => res.json())
-            .then(data => this.setState({
-                racers: data.MRData.StandingsTable.StandingsLists[0].DriverStandings
-            }))
-    }
+            .then(data => setRacers(data.MRData.StandingsTable.StandingsLists[0].DriverStandings))
+    })
 
-    render() {
-        console.log('Racers component rendered')
-        console.log(this.state)
-        return (
-            <div>
-                This is the Racers Page
-                {this.state.racers.map((r, i) => <RacerDetail key={i} racer={r} />)}
-            </div>
-        )
-    }
+    return (
+        <div>
+            This is the Racers Page
+            {racers.length ? 
+                <table className="table table-dark table-striped">
+                <thead>
+                    <tr>
+                        <td>Position</td>
+                        <td>Points</td>
+                        <td>Wins</td>
+                        <td>First Name</td>
+                        <td>Last Name</td>
+                        <td>Nationality</td>
+                        <td>Constructor</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {racers.map((r, i) => <RacerDetail key={i} racer={r} />)}
+                </tbody>
+            </table>
+            : null}
+            
+        </div>
+    )
 }
